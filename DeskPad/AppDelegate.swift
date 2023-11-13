f32ebec674792f2fa25c74e2dd902ba449402245
Earl Ramirez Sanchez <earlramirez@apple.com>
2023-11-13 10:56:19 -0800

    Add menu option to disable the ability to resize the window

--- DeskPad/AppDelegate.swift
+++ DeskPad/AppDelegate.swift
@@ -20,12 +20,34 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         window.backgroundColor = .white
         window.contentMinSize = CGSize(width: 400, height: 300)
         window.contentMaxSize = CGSize(width: 3840, height: 2160)
-        window.styleMask.insert(.resizable)
         window.collectionBehavior.insert(.fullScreenNone)
 
         let mainMenu = NSMenu()
         let mainMenuItem = NSMenuItem()
         let subMenu = NSMenu(title: "MainMenu")
+
+        // Menu item for isWindowResizeAllowed
+        let enableWindowResizeMenuItem = NSMenuItem(
+            title: "Enable ability to resize the window",
+            action: #selector(toggleWindowResizeEnable(_:)),
+            keyEquivalent: ""
+        )
+
+        if UserDefaults.standard.object(forKey: "isWindowResizeAllowed") == nil {
+            UserDefaults.standard.set(true, forKey: "isWindowResizeAllowed")
+        }
+
+        let isWindowResizeAllowed = UserDefaults.standard.bool(forKey: "isWindowResizeAllowed")
+        enableWindowResizeMenuItem.state = isWindowResizeAllowed ? .on : .off
+        subMenu.addItem(enableWindowResizeMenuItem)
+        // Apply current state
+        if isWindowResizeAllowed {
+            window.styleMask.insert(.resizable)
+        } else {
+            window.styleMask.remove(.resizable)
+        }
+
+        // Menu item for quit application
         let quitMenuItem = NSMenuItem(
             title: "Quit",
             action: #selector(NSApp.terminate),
@@ -41,5 +63,15 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationShouldTerminateAfterLastWindowClosed(_: NSApplication) -> Bool {
         return true
+    }
+
+    @objc func toggleWindowResizeEnable(_ sender: NSMenuItem) {
+        sender.state = (sender.state == .on) ? .off : .on
+        UserDefaults.standard.set(sender.state == .on, forKey: "isWindowResizeAllowed")
+        if sender.state == .on {
+            window.styleMask.insert(.resizable)
+        } else {
+            window.styleMask.remove(.resizable)
+        }
     }
 }
